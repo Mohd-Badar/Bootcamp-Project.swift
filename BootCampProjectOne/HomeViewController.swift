fa221088e53dc473100ce9c88e45392c5369e202
Mohd Badar <mohd.badar9917@gmail.com>
2026-07-11 11:37:40 +0530

    Update

--- BootCampProjectOne/HomeViewController.swift
+++ BootCampProjectOne/HomeViewController.swift
@@ -24,10 +24,44 @@ class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDe
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return UITableView.automaticDimension
     }
+    @IBAction func `switch`(_ sender: Any) {
+    }
+    @IBOutlet weak var msgBtn: UIButton!
+    @IBOutlet weak var lightBlueView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var blueViewBottomLabel: UILabel!
+    @IBOutlet weak var blueViewTopLabel: UILabel!
+    @IBOutlet weak var lightBlueViewBottomLabel: UILabel!
+    @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var bellIconBtn: UIButton!
+    @IBOutlet weak var lightBlueViewTopLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var locationImage: UIImageView!
     @IBOutlet var homeHeaderView: UIView!
     @IBOutlet weak var homeTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationImage.image = UIImage(named: "LocationIcon")
+        locationLabel.text = "A-199, Sector 63, No..."
+        locationLabel.textColor = UIColor(hexString: "#FFFFFF")
+        bellIconBtn.setImage(UIImage(named: "BellIcon"), for: .normal)
+        msgBtn.setImage(UIImage(named: "MsgIcon"), for: .normal)
+        bannerImage.image = UIImage(named: "HomeBanner")
+        blueView.backgroundColor = UIColor(hexString: "#4896FF")
+        blueView.layer.cornerRadius = 20
+        lightBlueView.backgroundColor = UIColor(hexString: "#E4EFFE")
+        lightBlueView.layer.cornerRadius = 20
+        blueViewTopLabel.text = "20"
+        blueViewBottomLabel.text = "Upcoming bookings"
+        blueViewTopLabel.textColor = UIColor(hexString: "#FFFFFF")
+        blueViewBottomLabel.textColor = UIColor(hexString: "#FFFFFF")
+        lightBlueViewTopLabel.text = "7"
+        lightBlueViewBottomLabel.text = "Upcoming RFP"
+        lightBlueViewTopLabel.textColor = UIColor(hexString: "#000D1E")
+        lightBlueViewBottomLabel.textColor = UIColor(hexString: "#000D1E")
+        
+        
         if let tabBarController = self.tabBarController {
             tabBarController.tabBar.isUserInteractionEnabled = true
         }
